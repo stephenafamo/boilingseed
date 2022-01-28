79c10e3a400c5c4f95e21e0c07cf2985caa0a8b5
Stephen Afam-Osemene <me@stephenafamo.com>
2022-01-28 23:31:00 +0100

    upgrade SQLBoiler

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.16
 require (
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/viper v1.10.1
-	github.com/volatiletech/sqlboiler/v4 v4.8.4
+	github.com/volatiletech/sqlboiler/v4 v4.8.5
 	golang.org/x/mod v0.5.0
 )
--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"io/fs"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 	"text/tabwriter"
@@ -152,24 +151,10 @@ func preRun(cmd *cobra.Command, args []string) error {
 		return commandFailure("must provide a driver name")
 	}
 
-	driverName := args[0]
-	driverPath := args[0]
-
-	if strings.ContainsRune(driverName, os.PathSeparator) {
-		driverName = strings.Replace(filepath.Base(driverName), "sqlboiler-", "", 1)
-		driverName = strings.Replace(driverName, ".exe", "", 1)
-	} else {
-		driverPath = "sqlboiler-" + driverPath
-		if p, err := exec.LookPath(driverPath); err == nil {
-			driverPath = p
-		}
-	}
-
-	driverPath, err = filepath.Abs(driverPath)
+	driverName, driverPath, err := drivers.RegisterBinaryFromCmdArg(args[0])
 	if err != nil {
-		return fmt.Errorf("could not find absolute path to driver: %w", err)
+		return fmt.Errorf("could not register driver: %w", err)
 	}
-	drivers.RegisterBinary(driverName, driverPath)
 
 	// Create the directior
 	tempTemplatesDir, err = ioutil.TempDir("", "boilingseed")
